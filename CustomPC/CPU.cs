2e67cee91ca5f900929fd0870a49143a43e64725
MSiliunas <marijus.siliunas@gmail.com>
2014-09-14 19:04:32 +0300

    CPU class created;

---
+++ CustomPC/CPU.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomPC
+{
+    class CPU :CheckedPart
+    {
+        string socket;
+        public string Socket
+        {
+            get { return socket; }
+            set { socket = value; }
+        }
+
+        public CPU(string socket)
+        {
+            this.socket = socket;
+        }
+
+        override public void isSocketCompatible()
+        {
+
+        }
+    }
+}
